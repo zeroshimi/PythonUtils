81e55640b265e01f884bdfad2950b2a757fb7439
末松慈陸 <>
2022-09-10 18:05:48 +0900

    json file 作成module

---
+++ JsonFileSave.py
@@ -0,0 +1,17 @@
+import json
+
+def JsonSave(fileName: str, data: any):
+    with open(fileName ,mode='w') as f:
+            json.dump({"result": data}, f, cls = MyEncoder)
+
+
+class MyEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, np.integer):
+            return int(obj)
+        elif isinstance(obj, np.floating):
+            return float(obj)
+        elif isinstance(obj, np.ndarray):
+            return obj.tolist()
+        else:
+            return super(MyEncoder, self).default(obj)
